42a5cd25a84e8b108485fac0171ff1bcea8f98d0
Harry Brown <harrybrown98@gmail.com>
2020-11-02 21:40:12 -0800

    Did some more work on the database implementation
    Split meter readings into two tables and did some filtering of the MPU
    data to only get the most essential data.

--- db/init.sql
+++ db/init.sql
@@ -1,144 +1,43 @@
 -- Assets
 -- Files: Fares NonRevVehicles/* and Power/POWER Meter Data (all).xlsx
 CREATE TABLE asset (
-    num         INT,
-    bartdept    VARCHAR(16),
-    description VARCHAR(128),
-    status      VARCHAR(28)
+    num         int,
+    bartdept    varchar(16),
+    description varchar(128),
+    status      varchar(28)
 );
 
 -- Files: Fares NonRevVehicles/* and Power/POWER Meter Data (all).xlsx
 CREATE TABLE meter_reading (
-    assetnum      INT,
-    metername     VARCHAR(28),
-    readingsource VARCHAR(28),
-    reading       INT,
-    delta         INT,
+    assetnum      int,
+    metername     varchar(28),
+    readingsource varchar(28),
+    reading       int,
+    delta         int,
     readingdate   date,
     enterdate     date
 );
 
--- Work orders
--- File: Power/POWER WOs 9-22-2018 to 9-21-2020.xlsx
-CREATE TABLE power_work_order (
-    wonum                 INT NOT NULL, -- work order number
-    description           VARCHAR(128),
-    detail_description    VARCHAR(128),
-    -- TODO most of the "alias" column has INTs but some look like "64-14-19150-2704"
-    alias                 VARCHAR(32),
-    location              VARCHAR(64),
-    loc_desc              VARCHAR(128),
-    worktype              VARCHAR(8),
-    asset_type            VARCHAR(16),
-    bartdept              VARCHAR(8),
-    status                VARCHAR(28),
-    reportdate            DATE,
-    actstart              DATE,
-    actfinish             DATE,
-    actual_labor_hours    FLOAT,
-    material_cost         FLOAT,
-    problem_code_desc     VARCHAR(64),
-    reason_to_repair_desc VARCHAR(64),
-    component_desc        VARCHAR(64),
-    part_failure_desc     VARCHAR(28),
-    work_accomp_desc      VARCHAR(28),
-    wl_date               DATE,
-    wl_summary            VARCHAR(256),
-    wl_summary_detail     VARCHAR(256)
-);
-
--- MPU Sheet
--- Omitting lots of fields from the excel spreadsheet
--- File: Monthy Project Updates - MPU/MPU_July 20_20200820.xlsm
 CREATE TABLE mpu (
-    id                           VARCHAR(14) NOT NULL, -- primary key
-    name                         VARCHAR(128),
-    short_name                   VARCHAR(128),
-    criteria_ranking             INT,
-    location                     VARCHAR(128),
-    sub_location                 VARCHAR(128),
-    district_location            VARCHAR(64),
-    bart_performing_design       BOOLEAN,
-    bart_performing_construction BOOLEAN,
-    project_plan                 VARCHAR(255),
-    asset_risk_register_id       VARCHAR(20),
-    budget_amount                INT,
-    expended_amount              INT,
-    funding_level                VARCHAR(32),
-    end_date                     DATE,
-    monthly_burn_rate            INT
-);
-
--- Selection Criteria sheet
--- File: Monthy Project Updates - MPU/MPU_July 20_20200820.xlsm
-CREATE TABLE mpu_selection_criteria (
-    id              VARCHAR(14) NOT NULL,
-    name            VARCHAR(128),
-    MPL_status      VARCHAR(8),
-    peoplesoft_pm   VARCHAR(128), -- Peoplesoft project manager
-    rr_funded       BOOLEAN,
-    fta_funded      BOOLEAN,
-    program_manager VARCHAR(128),
-
-    Performance_Against_Commitments INT,
-    External_Commitment             INT,
-    Dependency_On_Other_Projects    INT,
-    Environmental_Conditions        INT,
-    Public_Impact                   INT,
-    Amt_Of_BART_Row_Reqd            INT,
-    Req_for_Specialized_Equipment   INT,
-    Earned_Val_or_Forecase          INT,
-    Changes_Scope_Sched_Deadlines   INT,
-    Budget                          INT,
-    CNI_Ranking                     INT,
-    Proj_Documentation              INT,
-    total_score                     INT
-);
-
--- See file 'Monthly Project Update - MPU/MPU_July 20_20200820.xlsx'
--- This is a subtable that is part of 'mpu_selection_criteria'
--- File: Monthy Project Updates - MPU/MPU_July 20_20200820.xlsm
-CREATE TABLE mpu_score_criteria (
-    number INT, -- Criteria #4, Criteria #5, ect.
-    weight INT
-);
-
--- EXPFUNDS sheet
--- no columns skipped
--- File: Monthy Project Updates - MPU/MPU_July 20_20200820.xlsm
-CREATE TABLE mpu_expiring_funds (
-    id                  VARCHAR(14) NOT NULL,
-    activity            VARCHAR(128),
-    seq                 INT,
-    rate                INT,
-    gl_unit             VARCHAR(32),
-    fund                VARCHAR(8),
-    descr               VARCHAR(32),
-    tgt_activity_id     VARCHAR(10),
-    category            VARCHAR(14),
-    threshold_amount    INT,
-    distribution_amount INT,
-    remaining_amount    INT,
-    fund_dst_status     VARCHAR(2),
-    seq2                INT,
-    adjust              VARCHAR(2),
-    seq_trans_id        INT,
-    end_date            DATE,
-    project_manager     VARCHAR(32),
-    department          VARCHAR(64)
-);
-
--- MILESTONES sheet
--- no columns skipped
--- File: Monthy Project Updates - MPU/MPU_July 20_20200820.xlsm
-CREATE TABLE mpu_milestones (
-    id            VARCHAR(14) NOT NULL,
-    name          VARCHAR(128), -- TODO this is reapeated data, can be found in mpu table
-    phase         VARCHAR(32),
-    task          VARCHAR(32),
-    schedule_code VARCHAR(20),
-    activity_name VARCHAR(64),
-    currentdate   DATE,
-    baseline_date DATE,
-    days_ahead    INT
+    id                varchar(14),
+    name              varchar(128),
+    short_name        varchar(128),
+    ranking           int,
+    description       varchar(128),
+    location          varchar(32),
+    sub_location      varchar(32),
+    district_location varchar(64),
+    mpu_phase         varchar(32),
+    budget_amount     float,
+    expended_amount   float,
+    monthly_burn_rate float,
+    remaining_budget  float,
+    funding_level     varchar(16),
+    rr_funded         boolean,
+    project_group     varchar(64),
+    project_manager   varchar(32),
+    accomplishments   varchar(128),
+    program           varchar(32),
+    review_format     varchar(32),
+    end_date          date
 );
--- db/load.sql
+++ db/load.sql
@@ -1,27 +1,32 @@
+.mode "csv"
+.separator ","
+
+-- Loading MPUs
+.import "UC Merced 2020 SE Project/Monthly Project Update - MPU/mpu.csv" "mpu"
+
+-- Loading Meter data
 CREATE TABLE meter_reading_tmp (
     bartdept      varchar(8),
-    assetnum      INT,
-    description   VARCHAR(128),
-    status        VARCHAR(28),
-    metername     VARCHAR(28),
-    readingsource VARCHAR(28),
-    reading       INT,
-    delta         INT,
+    assetnum      int,
+    description   varchar(128),
+    status        varchar(28),
+    metername     varchar(28),
+    readingsource varchar(28),
+    reading       int,
+    delta         int,
     readingdate   date,
     enterdate     date
 );
-
-.mode "csv"
-.separator ","
-.import "bart_data/Fares NonRevVehicles/all_meterdata.csv" "meter_reading_tmp"
+-- load the data into a temp table
+.import "UC Merced 2020 SE Project/Fares NonRevVehicles/all_meterdata.csv" "meter_reading_tmp"
 
 INSERT INTO asset
     SELECT assetnum as num, bartdept, description, status
     FROM meter_reading_tmp
     GROUP BY num;
-
 INSERT INTO meter_reading
     SELECT assetnum, metername, readingsource, reading, delta, readingdate, enterdate
     FROM meter_reading_tmp;
-
 DROP TABLE meter_reading_tmp;
+
+VACUUM; -- clean up the temp data
