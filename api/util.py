f59eb139c7bec5ba2c313056670f7148177e2a28
Harry Brown <harrybrown98@gmail.com>
2020-11-09 15:02:28 -0800

    Wrote a json encoder that supports our database models

--- api/util.py
+++ api/util.py
@@ -1,5 +1,31 @@
 import traceback
 
+from .asset.models import Asset, MeterReading
+import flask.json
+
+
+class ModelEncoder(flask.json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Asset):
+            return {
+                'num': obj.num,
+                'bartdept': obj.bartdept,
+                'description': obj.description,
+                'status': obj.status,
+            }
+        elif isinstance(obj, MeterReading):
+            return {
+                'assetnum': obj.assetnum,
+                'metername': obj.metername,
+                'readingsource': obj.readingsource,
+                'reading': obj.reading,
+                'delta': obj.delta,
+                'readingdate': obj.readingdate,
+                'enterdate': obj.enterdate,
+            }
+        return super().default(obj)
+
+
 def _json_exception(e: Exception, status_code=500):
     return {
         'error': 'internal server error',
